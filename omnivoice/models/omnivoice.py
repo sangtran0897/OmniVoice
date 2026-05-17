22f17e8c36b2a0f0779485dca33c8d89db003738
sang lenovo <sangtran0897@gmail.com>
2026-05-17 15:37:22 +0700

    edit code

--- omnivoice/models/omnivoice.py
+++ omnivoice/models/omnivoice.py
@@ -766,7 +766,7 @@ class OmniVoice(PreTrainedModel):
             generated_audio = remove_silence(
                 generated_audio,
                 self.sampling_rate,
-                mid_sil=1800,
+                mid_sil=500,
                 lead_sil=100,
                 trail_sil=100,
             )
